03f964fbb1eefe4c4a3d8d3c68db330184130a08
phamthehieu <111071418+phamthehieu@users.noreply.github.com>
2023-02-23 11:21:08 +0700

    user-login

--- dist/src/router/router.js
+++ dist/src/router/router.js
@@ -2,5 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
+const authRouter_1 = require("./authRouter");
+const userRouter_1 = require("./userRouter");
+const auth_1 = require("../middleware/auth");
 exports.router = (0, express_1.Router)();
+exports.router.use('/auth', authRouter_1.authRouter);
+exports.router.use(auth_1.auth);
+exports.router.use('/users', userRouter_1.userRouter);
 //# sourceMappingURL=router.js.map
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,2 +1,8 @@
 import {Router} from "express";
+import {authRouter} from "./authRouter";
+import {userRouter} from "./userRouter";
+import {auth} from "../middleware/auth";
 export const router = Router()
+router.use('/auth', authRouter)
+router.use(auth)
+router.use('/users', userRouter)
